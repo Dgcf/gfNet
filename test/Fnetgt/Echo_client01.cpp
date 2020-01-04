19dcee73de72c07304d9d313fd17863436a7ecc5
gongfeng <feng.gong@ecarx.com.cn>
2020-01-05 00:39:37 +0800

    Update: 1.实现多线程多连接,但存在长时间运行后崩溃bug 2.log打印存在崩溃 3.断开连接未测试,服务端踢掉连接未实现

--- test/Fnetgt/Echo_client01.cpp
+++ test/Fnetgt/Echo_client01.cpp
@@ -1,2 +1,101 @@
-#include <sys/socket.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <future>
+using namespace std;
+
+class TcpClient
+{
+public:
+    TcpClient():
+    v_{"hello","world","good","morning"},
+    count_(0),
+    sock_(0)
+    {
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        addr.sin_port = htons(37777);
+
+        sock_ = socket(PF_INET, SOCK_STREAM, 0);
+        if (-1 == sock_)
+        {
+            printf("create socket fail \n");
+        }
+    }
+
+    int connec()
+    {
+        if (-1 == connect(sock_, (sockaddr*)&addr, sizeof(addr)))
+        {
+            printf("connect fail\n");
+            return -1;
+        }
+        printf("#############New connect, sock is : %d##############\n", sock_);
+        return 0;
+    }
+
+    void start()
+    {
+        int ret = connec();
+        if (ret == -1)
+        {
+            return;
+        }
+        
+        char read_buf[1024];
+        int read_num = 0;
+
+        while (1)
+        {
+            if(count_ >= v_.size())
+            {
+                count_ = 0;
+            }
+            if(-1 == write(sock_, v_[count_].c_str(), sizeof(v_[count_])))
+            {
+                printf("write fail\n");
+                continue;
+            }
+            memset(read_buf, 0, sizeof(read_buf));
+            read_num = read(sock_, read_buf, 32);
+            printf("read bytes: %d, buf: %s\n", read_num, read_buf);
+            sleep(5);
+        }
+    }
+
+private:
+    sockaddr_in addr;
+    int sock_;
+    int count_;
+    std::vector<string> v_;
+};
+
+void test()
+{
+    TcpClient client;
+    client.start();
+}
+
+int main()
+{
+    const int count = 1000;
+    std::future<void> results[count];
+    for (size_t i = 0; i < count; i++)
+    {
+        results[i] = std::async(test);
+        sleep(1);
+    }
+
+    for (size_t i = 0; i < count; i++)
+    {
+        results[i].wait();
+    }
+    
+}
--- test/Fnetgt/Echo_server01.cpp
+++ test/Fnetgt/Echo_server01.cpp
@@ -2,28 +2,33 @@
 #include <stdlib.h>
 #include "../../src/net/fnetgt/inc/TcpServer.h"
 #include "../../src/net/fnetgt/inc/fnetgt.h"
+#include "../../src/base/Log/inc/Logger.h"
+#include "../../src/base/Thread/inc/thread.h"
 
 using namespace gNet::Fnetgt;
+using namespace gNet::CurrentThread;
 
 void NewConnectionNotify()
 {
     printf("New Connected! \n");
 }
 
+
 void OnMessageCallback(const TcpConnectionPtr& _ptr, const char* _msg, int _len)
 {
-    printf("Enter in OnMessageCallback\n");
+    // printf("Enter in OnMessageCallback\n");
     string s = _msg;
     // char sz[1024];
     // strncpy(sz, _msg, _len);
-    printf("Received msg is : %s\n", s.c_str());
+    printf("Current: %s, Received msg is : %s\n", gNet::CurrentThread::ICurrentthreadID().c_str(), s.c_str());
+    //LOG_INFO<<s.c_str();
     s += "ECARX_TEST";
     _ptr->Send(s.c_str(), s.length());
 }
 
 int test0()
 {
-    gNet::Fnetgt::TcpServer server("127.0.0.1", 37777);
+    gNet::Fnetgt::TcpServer server("127.0.0.1", 37777, 10);
     server.SetNewCollectionCallback(NewConnectionNotify);
     server.SetMessageCallback(OnMessageCallback);
     server.Start();
